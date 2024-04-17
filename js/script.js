4b4f80a24b75f6d073f47c9dab6610b17fe25d11
johnterry27 <161942169+johnterry27@users.noreply.github.com>
2024-04-17 11:53:29 +0700

    Finishing

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 // Toggel Class Active
 const navbarNav = document.querySelector(".navbar-nav");
-// Menu
+
+// Ketika menu di kilk
 document.querySelector("#menu1").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// nav
+// klik di luar sidebar untuk menghilngkan nav
 const manu1 = document.querySelector("#menu1");
 
 document.addEventListener("click", function (e) {
